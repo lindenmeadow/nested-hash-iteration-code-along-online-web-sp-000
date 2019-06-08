27cfd903f3608859315d29a5e1387d33e98af19d
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-06-08 15:43:35 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,9 +15,9 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts.each do |person, data|
+  contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
-      data.each do |attribute, value|
+      contact_details_hash.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
-          value.delete_if {|ice_cream| ice_cream == "strawberry"}
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
 end
